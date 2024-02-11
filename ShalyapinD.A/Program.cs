5aa6217c0934f0247b7214bea2b3bb84230d5043
Danila Shalyapin <dashoff@yandex.ru>
2024-02-11 10:28:45 +0400

    1

---
+++ ShalyapinD.A/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShalyapinD.A
+{
+    internal class Program
+    {
+        private static float num_1, num_2;
+        private static float result;
+
+        private static string operation;
+        private static string choiceContinue;
+
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("Введите первое число");
+                num_1 = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число");
+                num_2 = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Введите операцию");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        result = num_1 + num_2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+
+                    case "-":
+                        result = num_1 - num_2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+
+                    case "/":
+                        if (num_2 != 0)
+                        {
+                            result = num_1 / num_2;
+                            Console.WriteLine("Результат: " + result);
+                        }
+                        else Console.WriteLine("Делить на 0 нельзя");
+                        break;
+
+                    case "*":
+                        result = num_1 * num_2;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+
+                }
+
+                Console.Write("Хотите продолжить? (y/n): ");
+                choiceContinue = Console.ReadLine();
+
+            }
+            while (choiceContinue == "y" || choiceContinue == "Y");
+        }
+    }
+}
---
+++ ShalyapinD.A/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ShalyapinD.A")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShalyapinD.A")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0696714c-1b94-4e2f-943d-92577322264b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
